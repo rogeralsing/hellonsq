8c084021e8612b80ee54a57605b387c72d5193c2
rogeralsing <rogeralsing@gmail.com>
2016-03-24 10:23:00 +0100

    bar

--- main.go
+++ main.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"log"
+	"github.com/bitly/go-nsq"
+	"sync"
+)
+
 func main() {
+	config := nsq.NewConfig()
+	w, _ := nsq.NewProducer("127.0.0.1:4150", config)
+
+	err := w.Publish("write_test", []byte("test"))
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+
+	w.Stop()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	q, _ := nsq.NewConsumer("write_test", "ch", config)
+	q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		log.Printf("Got a message: %v", message)
+		wg.Done()
+		return nil
+	}))
+	err = q.ConnectToNSQD("127.0.0.1:4150")
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+	wg.Wait()
 }
